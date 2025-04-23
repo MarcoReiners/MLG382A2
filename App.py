58a917e3a64bf54bd1166516a0165569bc96008d
MarcoReiners <marcoreiners816@gmail.com>
2025-04-23 19:26:28 +0200

    segvczxxhnjdsfhsdf

--- App.py
+++ App.py
@@ -89,6 +89,8 @@ def predict(n_rf, n_xgb, n_logreg, *values):
 
 # Run app
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    print("Launching Dash app...")
+    try:
+        app.run(debug=True, host='127.0.0.1', port=8050)
+    except Exception as e:
+        print("Failed to start server:", e)
